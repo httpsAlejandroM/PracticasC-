9a4f4d0762101fd3628e05ca6ffc3f683f9c72ed
Alejandro Medina <104036331+httpsAlejandroM@users.noreply.github.com>
2025-05-19 20:51:58 -0300

    Add files via upload

---
+++ Guia de ejercicios Nro 3/Ejercicio 6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, numero, contador = 0;
+
+    cout << "Indique cantidad de n£meros: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++){
+       cout << "Ingrese un numero: ";
+       cin >> numero;
+
+       if(numero > 0){
+          contador++;
+       }
+    }
+
+    cout << "Cantidad de positivos: " << contador << endl;
+
+    return 0;
+}
+
